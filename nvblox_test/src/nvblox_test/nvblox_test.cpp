dba4e8194d0dd0c20e1eb17531b1dce929439017
Helen Oleynikova <helenoleynikova@gmail.com>
2024-01-24 10:59:03 +0100

    Full test node.

--- nvblox_test/src/nvblox_test/nvblox_test.cpp
+++ nvblox_test/src/nvblox_test/nvblox_test.cpp
@@ -1,5 +1,29 @@
-#include "nvblox/nvblox.h"
+#include <glog/logging.h>
+#include <memory>
 
-int main(){
-	return 0;
+#include <ros/ros.h>
+
+#include <nvblox/core/internal/warmup_cuda.h>
+
+#include <nvblox_ros/nvblox_node.hpp>
+
+using namespace std::chrono_literals;
+
+int main(int argc, char* argv[]) {
+  google::InitGoogleLogging(argv[0]);
+  FLAGS_alsologtostderr = true;
+  google::InstallFailureSignalHandler();
+  ros::init(argc, argv, "nvblox_node_test");
+  ros::NodeHandle nh, nh_private("~");
+
+  // Warmup CUDA so it doesn't affect our timings *as* much for the first
+  // CUDA call.
+  nvblox::warmupCuda();
+
+  nvblox::NvbloxNode node(nh, nh_private);
+
+  ros::spin();
+
+  ros::shutdown();
+  return 0;
 }
